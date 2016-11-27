7743fc1aabc5915e96a8dad823b029dfbb7b80d9
kumangkumeng <kumangxxx@gmail.com>
2016-11-27 22:51:13 +0700

    first3

--- app/index.js
+++ app/index.js
@@ -1,6 +1,13 @@
+var express = require('express');
+var app = express();
+
 console.log("DB IP : " + process.env.dbip);
 
 var mysql = require('mysql');
+
+app.listen((process.env.PORT || 8082), function() {
+console.log("running..");
+});
 
 var pool = mysql.createPool({
 	localAddress : process.env.dbip,
