f68a31e48cfbd9e7fc425c0a365f25cca49ea698
kumangkumeng <kumangxxx@gmail.com>
2016-11-27 23:04:02 +0700

    first4

--- app/index.js
+++ app/index.js
@@ -1,26 +1,24 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 
 console.log("DB IP : " + process.env.dbip);
 
-var mysql = require('mysql');
-
 app.listen((process.env.PORT || 8082), function() {
-console.log("running..");
-});
+	console.log("running..");
+	var pool = mysql.createPool({
+		localAddress : process.env.dbip,
+		user : "root",
+		password : "gitsr4h4514"
+	});
 
-var pool = mysql.createPool({
-	localAddress : process.env.dbip,
-	user : "root",
-	password : "gitsr4h4514"
+	pool.getConnection(function(err, c) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(c);
+		}
+	});
 });
-
-//pool.getConnection(function(err, c) {
-//if (err) {
-//console.log(err);
-//} else {
-//console.log(c);
-//}
-//});
 
 console.log("end");
